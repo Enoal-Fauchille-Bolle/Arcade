7e4847e60a36a11da22eee5e3e44febefdef38e6
You <tom.feldkamp@epitech.eu>
2025-03-28 15:43:26 +0100

    remove: :fire: | useless datas

--- src/interfaces/Idatas.hpp
+++ src/interfaces/Idatas.hpp
@@ -21,29 +21,6 @@ struct GameData {
     std::string user;
     std::string lib;
     float score;
-}:
-
-class IDatas {
-    public:
-        virtual ~IDatas() = default;
-
-
-        virtual void ParseEntities(std::vector<Entity> entities) = 0;
-        virtual std::vector<Entity> getEntities(void) = 0;
-        virtual void sendEntities(std::vector<Entity> entities) = 0;
-        virtual void ClearEntites(std::vector<Entity> entities) = 0;
-
-        virtual void sendScore(int score) = 0;
-        virtual int getScore(void) = 0;
-        virtual std::string getLibName(void) = 0;
-        virtual std::string getGameName(void) = 0;
-
-    protected:
-
-        std::vector<Entity> _entities;
-
-    private:
 };
-
 
 #endif /* !IDATAS_HPP_ */
