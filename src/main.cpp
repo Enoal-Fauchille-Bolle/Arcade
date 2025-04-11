e3dfedddae036f47311dd545072456d72a6b2f93
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-04-11 17:56:22 +0200

    feat(help): ✨ | add controls section to help output
    adds key bindings for user interaction with the display
    enhances the help output by including control commands
    improves user experience by providing essential gameplay shortcuts

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,18 @@ static void print_help()
     std::cout << "\t./arcade lib" << std::endl;
     std::cout << "DESCRIPTION" << std::endl;
     std::cout << "\tpath\tPath to the display library" << std::endl;
+    std::cout << "CONTROLS" << std::endl;
+    std::cout << "\tF1: Previous Sprite Set" << std::endl;
+    std::cout << "\tF2: Next Sprite Set" << std::endl;
+    std::cout << "\tF3: Previous Display Lib" << std::endl;
+    std::cout << "\tF4: Next Display Lib" << std::endl;
+    std::cout << "\tF5: Reload Game & Display Lib" << std::endl;
+    std::cout << "\tF6: Reload Game" << std::endl;
+    std::cout << "\tF7: Reload Display Lib" << std::endl;
+    std::cout << "\tF8: Next Display Lib" << std::endl;
+    std::cout << "\tF9: Next Display Lib" << std::endl;
+    std::cout << "\tF10: Go back to Menu" << std::endl;
+    std::cout << "\tF11: Exit" << std::endl;
 }
 
 int main(int ac, char **av)
